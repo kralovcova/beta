6d1c8ee6e33afc58e276a5335c8ca62b8245d5f0
kralovcova <jirina.kralovcova@tul.cz>
2021-10-07 12:03:14 +0200

    Kralovcova - echo cisla

---
+++ src/kralovcova/EchoCisla.java
@@ -0,0 +1,26 @@
+/*
+This code is part of ALG subject
+ */
+
+package kralovcova;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jirina.kralovcova
+ */
+public class EchoCisla {
+
+    private static Scanner sc = new Scanner(System.in);
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Zadej cislo");
+        int c = sc.nextInt();
+        System.out.println("Zadane cislo " + c);
+    }
+    
+}
